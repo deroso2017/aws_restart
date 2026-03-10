299d27972d969d02a4b4a56c5c68ebdfae4d4d11
Behdad Tabrizi <b.tabrizi@deroso.de>
2026-03-10 11:22:45 +0100

    add caculate prime number function

---
+++ calculate-prime-numbers.py
@@ -0,0 +1,27 @@
+import os
+
+# Get the folder where this Python file is located
+# __file__ It contains the full path of the current Python file
+base_dir = os.path.dirname(__file__)
+input_path = os.path.join(base_dir, "files", "results.txt")
+output_path = os.path.join(base_dir, "files", "results.txt")
+
+prime_numbers = []
+
+
+def calc_primes(start, end):
+    for num in range(start, end + 1):
+        is_prime = True
+        for i in range(2, num):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            prime_numbers.append(num)
+    with open(output_path, "w") as file:
+        file.write(", ".join(map(str, prime_numbers)))
+        print("result.txt created successfully")
+        print(prime_numbers)
+
+
+calc_primes(15, 250)
